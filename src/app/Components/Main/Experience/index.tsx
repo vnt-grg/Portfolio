9c680ba4b89d3cbaa78e85f2efb2892c6377485a
Vineet Garg <vinetgrag@gmail.com>
2025-11-01 22:47:47 +0530

    Update index.tsx

--- src/app/Components/Main/Experience/index.tsx
+++ src/app/Components/Main/Experience/index.tsx
@@ -8,7 +8,7 @@ const Experience = () => {
     <div>
       <div className="p-8">
         <div className="w-[80%] md:w-[28%] md:ml-[72%]">
-          <a href="/VINEET GARG.pdf" download>
+          <a href="/Resume Vineet.pdf" download>
             <div className="flex text-[#FFDB70] shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] py-3 px-5 rounded-lg bg-gradient-to-r from-[#FFDB70]/30 to-[#202022]">
               <IoIosFolderOpen size={20} />
               <p className="pl-2">Download Resume</p>
